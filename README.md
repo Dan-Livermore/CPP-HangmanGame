4b1be260f4bf482817901c290c02ade70a7a8432
Dan-Livermore <danieljlivermore@gmail.com>
2025-02-21 15:09:19 +0000

    Installed C++ locally and started creating variables

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CPP-HangmanGame
-Used to get used to syntax of C++
+Used to get used to syntax of C++.
+
+Updates:
+- Finish Game
+- Add New Words to Word Pool
+- Edit / Remove Words From Pool
+- Difficulty Settings Based On Turn Count
+- GUI
+
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    std::string words[7] = {"sports", "music", "play", "chess", "fit", "party", "motion"};
+    std::string chosenWord []= words[1];
+    std::string hangman;
+    bool GameStarted = false;
+    
+    std::cout << "Welcome to C++ Hangman Game!";
+    return 0;
+}
