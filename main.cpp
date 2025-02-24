56afa52e0a43d332d1bc06b6f473951ccae1b730
Dan-Livermore <danieljlivermore@gmail.com>
2025-02-24 16:57:01 +0000

    Fixed local configuration errors, can now debug

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-
+using namespace std;
 int main(){
-    std::string words[7] = {"sports", "music", "play", "chess", "fit", "party", "motion"};
-    std::string chosenWord []= words[1];
-    std::string hangman;
+    // std::string words[7] = {"sports", "music", "play", "chess", "fit", "party", "motion"};
+    // std::string chosenWord = words[1];
+    // std::string hangman;
     bool GameStarted = false;
     
-    std::cout << "Welcome to C++ Hangman Game!";
+    std::cout << "Welcome to C++ Hangman Game!" << std::endl;
+    std::cout << "Press Any Key To Continue" << std::endl;
+    std::cin.get();
+    GameStarted = true;
+    std::cout << "Game Start!" << std::endl;
+
     return 0;
 }
