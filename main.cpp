af05d1fde3e233a7762ee15ceb4c2c646f8f4da8
Dan-Livermore <danieljlivermore@gmail.com>
2025-03-03 14:41:05 +0000

    Completed base functionality

--- main.cpp
+++ main.cpp
@@ -10,16 +10,47 @@ bool GameRunning = false;
 bool GameStarted = false;
 int turn;
 
+void updateScore(bool found){
+    if (hangman == chosenWord){
+        cout << "You won!" << endl;
+        GameStarted = false;
+    }
+    if (turn == 9){
+        cout << "Game Over!" << endl;
+        GameStarted = false;
+    };    
+    cout << "" << endl;
+    turn++;
+}
+
+void validateGuess(char guess){
+    bool found = false;
+    for (int i = 0; i < hangmanLength; i++) {
+        // If letter is in the word, update hangman. Keep going for duplicate characters
+        if (chosenWord[i] == guess) {
+            hangman[i] = guess;
+            found = true;
+        }
+    }
+    cout << hangman << endl;
+    updateScore(found);
+}
+
+void playerGuesses(){
+    cout << "Hangman: " << hangman << endl;
+    char guess;
+    cout << "Enter a letter: ";
+    cin >> guess;
+    // cout << "You guessed: " << guess << endl;
+    validateGuess(guess);
+}
+
 void prepareHangman(char hangman[]){
-    int hangmanLength = chosenWord.length();
+    hangmanLength = chosenWord.length();
     for (int i = 0; i < hangmanLength; i++) {
         hangman[i] = '_';
     }
     hangman[hangmanLength] = '\0'; // Limits the char array to length of the word
-
-    cout << "Word length: " << hangmanLength << endl;
-    cout << "Word selected: " << hangman << endl;
-    cout << "Word selected: " << chosenWord << endl;
 }
 
 void selectWord(string &chosenWord){
@@ -38,6 +69,16 @@ void initGame(){
 
 void startup(bool &GameRunning){
     cout << "Welcome to C++ Hangman Game!" << endl;
+    std::cout << R"(
+         __    __       ___       __   __    _______  ___  ___       ___       __   __       _______      ___       ___  ___   _______  __  
+        |  |  |  |     /   \     |  \ |  |  /  _____||   \/   |     /   \     |  \ |  |     /  _____|    /   \     |   \/   | |   ____||  | 
+        |  |__|  |    /  ^  \    |   \|  | |  |  __  |  \  /  |    /  ^  \    |   \|  |    |  |  __     /  ^  \    |  \  /  | |  |__   |  | 
+        |   __   |   /  /_\  \   |       | |  | |_ | |  |\/|  |   /  /_\  \   |       |    |  | |_ |   /  /_\  \   |  |\/|  | |   __|  |  | 
+        |  |  |  |  /  _____  \  |  |\   | |  |__| | |  |  |  |  /  _____  \  |  |\   |    |  |__| |  /  _____  \  |  |  |  | |  |____ |__| 
+        |__|  |__| /__/     \__\ |__| \__|  \______| |__|  |__| /__/     \__\ |__| \__|     \______| /__/     \__\ |__|  |__| |_______|(__) 
+                                                                                                                                            
+          )" << std::endl;
+
     cout << "Press Enter To Continue" << endl;
     cin.ignore();
     GameRunning = true;
@@ -49,6 +90,7 @@ int main(){
     while (GameRunning){
         initGame();
         while (GameStarted){
+            playerGuesses();
             // Player Guesses
             // Check if letter is in the word
             // If letter is in the word, replace the underscore with the letter
